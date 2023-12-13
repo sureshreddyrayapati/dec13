c342dea4e89b420e87949f16d5d5bb56a7e759dc
sureshreddyrayapati <resuresh0@gmail.com>
2023-12-13 12:26:48 +0530

    Add project files.

---
+++ dec13/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dec13
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hii suresh");
+            Console.ReadLine();
+        }
+    }
+}
